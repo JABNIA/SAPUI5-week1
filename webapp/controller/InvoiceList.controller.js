6d88f318580b2b983d4cb653eb82eabf1c5508d6
JABNIA <vaxojabnia@gmail.com>
2025-11-06 16:54:27 +0400

    filtering with status is available

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -3,12 +3,25 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], (Controller, JSONModel, Filter, FilterOperator) => {
+    "sap/ui/model/FilterOperator",
+], (Controller, JSONModel, Filter, FilterOperator, Formatter) => {
     "use strict"
 
     return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
         onInit() {
+            const oInvoice = new JSONModel();
+            this.formater = Formatter
+            oInvoice.loadData("../Invoices.json");
+            oInvoice.attachRequestCompleted(() => {
+                const statusSet = new Set(["All", ...oInvoice.getData().Invoices.map(e => e.Status)])
+                const statuses = Array.from(statusSet)
+                
+                const oStatusModel = new JSONModel({
+                    statusCollection: statuses.map(status => {return {key: status, text: status}})
+                })
+                this.getView().setModel(oStatusModel, "allStatuses");
+            });
+
             const oViewModel = new JSONModel({
                 currency: "EUR"
             });
@@ -35,6 +48,24 @@ sap.ui.define([
                 invoicePath: window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substring(1))
             }
             );
-        }
-    });
+        },
+
+        onSelectionChange(oEvent){
+            const aFilter = [];
+            const sStatusCode = oEvent.getParameter("selectedItem").getKey()
+
+            const oList = this.byId("invoiceList");
+            const oBinding = oList.getBinding("items");
+
+            if (sStatusCode && sStatusCode !== "All") {
+                const oStatusFilter = new sap.ui.model.Filter({
+                    path: "Status",
+                    operator: sap.ui.model.FilterOperator.EQ,
+                    value1: sStatusCode
+                })
+                aFilter.push(oStatusFilter);
+            }
+            oBinding.filter(aFilter, sap.ui.model.FilterType.Application)
+    }
+});
 }); 
